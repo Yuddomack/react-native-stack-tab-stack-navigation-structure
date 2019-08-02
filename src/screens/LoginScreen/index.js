78304b0bd64a19f09fdd47b634f93d0d6189cc5f
yuddomack <yuddomack@Yuddomack-ui-MacBookPro.local>
2019-08-02 14:29:50 +0900

    add: LoginScreen

---
+++ src/screens/LoginScreen/index.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default class LoginScreen extends Component{
+    
+    static navigationOptions = {
+        header: null,
+    };
+
+    _doLogin(){
+        // do something
+        this.props.navigation.replace('TabNavigator')
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <View style={styles.titleArea}>
+                    <Text style={styles.title}>Simple App</Text>
+                </View>
+                <View style={styles.formArea}>
+                    <TextInput 
+                        style={styles.textForm} 
+                        placeholder={"ID"}/>
+                    <TextInput 
+                        style={styles.textForm} 
+                        placeholder={"Password"}/>
+                </View>
+                <View style={styles.buttonArea}>
+                    <TouchableOpacity 
+                        style={styles.button}
+                        onPress={this._doLogin.bind(this)}>
+                        <Text style={styles.buttonTitle}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingLeft: wp('10%'),
+        paddingRight: wp('10%'),
+        justifyContent: 'center',
+    },
+    titleArea: {
+        width: '100%',
+        padding: wp('10%'),
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: wp('10%'),
+    },
+    formArea: {
+        width: '100%',
+        paddingBottom: wp('10%'),
+    },
+    textForm: {
+        borderWidth: 0.5,
+        borderColor: '#888',
+        width: '100%',
+        height: hp('5%'),
+        paddingLeft: 5,
+        paddingRight: 5,
+        marginBottom: 5,
+    },
+    buttonArea: {
+        width: '100%',
+        height: hp('5%'),
+    },
+    button: {
+        backgroundColor: "#46c3ad",
+        width: "100%",
+        height: "100%",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonTitle: {
+        color: 'white',
+    },
+})
